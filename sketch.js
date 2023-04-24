16071f7357db296330e33a50b8d6468c2fd8f032
victorsilvasonja <victordasilvasonjaferreira252gmail.com>
2023-04-24 20:08:52 -0300

    projeto11

--- sketch.js
+++ sketch.js
@@ -1,16 +1,30 @@
+var navio,naviomg
+var mar,marmg
 
 function preload(){
-
+naviomg=loadAnimation("./ship-1.png","./ship-2.png","./ship-3.png","./ship-4.png")
+marmg=loadImage("./sea.png")
 }
 
 function setup(){
   createCanvas(400,400);
+  mar=createSprite(200,200)
+  mar.addImage(marmg)
+  mar.scale=0.3
+  mar.velocityX=-3
+
+  
+  navio=createSprite(200,200)
+  navio.addAnimation("navio",naviomg)
+  navio.scale=0.3
   
 }
 
 function draw() {
   background("blue");
+  if (mar.x<0){
+    mar.x=mar.width/8
+    
+  }
     drawSprites();
-
- 
 }
